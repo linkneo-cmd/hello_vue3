801b4678e7a3079691962b8abac03842ab7df959
linkneo-cmd <1214916504@qq.com>
2024-04-27 10:02:08 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>你好啊！</h1>
+    <Person />
+  </div>
+</template>
+
+<script lang="ts">
+  import Person from './components/Person.vue';
+  export default {
+    name: 'App', // 组件名称
+    components: { // 注册子组件
+      Person
+    }
+  }
+</script>
+
+<style>
+    .app {
+        background-color: #ddd;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+        font-size: 20px;
+    }
+</style>
